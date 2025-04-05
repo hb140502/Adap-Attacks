56c1eac90ea4ba44f9d0bea1cdc6545c99cd2dfc
Henk <hb130502@outlook.com>
2025-04-05 10:38:45 +0200

    Remove clean images from training dataset to save space

--- train.sh
+++ train.sh
@@ -3,7 +3,6 @@
 . ./input_validation.sh
 input_validation $@
 
-repo_dir="$HOME/master-thesis/code/backdoorbench"
 my_dir="/vol/csedu-nobackup/project/hberendsen"
 data_dir="$my_dir/data"
 record_dir="$my_dir/record"
@@ -20,26 +19,28 @@ else
     attack_opts="-poison_rate=$pratio -cover_rate=$cratio"
 fi
 
-echo $attack_opts
-# python create_poisoned_set.py -dataset=$dataset -poison_type=$attack -data_dir=$data_dir -save_dir=$record_dir/$attack_id $attack_opts
+python create_poisoned_set.py -dataset=$dataset -poison_type=$attack -data_dir=$data_dir -save_dir=$record_dir/$attack_id $attack_opts
 
-# # Handle poisoned set failure
-# if [[ $? -ne 0 ]]; then
-#     mv "$record_dir/$attack_id" "$record_dir/FAIL_${attack_id}_${timestamp}"
-#     echo "!!! POISONED DATASET CREATION FAILURE !!!"
-#     exit 1
-# fi
+# Handle poisoned set failure
+if [[ $? -ne 0 ]]; then
+    mv "$record_dir/$attack_id" "$record_dir/FAIL_${attack_id}_${timestamp}"
+    echo "!!! POISONED DATASET CREATION FAILURE !!!"
+    exit 1
+fi
 
-# python train_on_poisoned_set.py -dataset=$dataset -poison_type=$attack -save_dir=$record_dir/$attack_id $attack_opts
+python train_on_poisoned_set.py -dataset=$dataset -poison_type=$attack -save_dir=$record_dir/$attack_id $attack_opts
 
-# # Handle training failure
-# if [[ $? -ne 0 ]]; then
-#     mv "$record_dir/$attack_id" "$record_dir/FAIL_${attack_id}_${timestamp}"
-#     echo "!!! TRAINING FAILURE !!!"
-#     exit 1
-# fi
+# Handle training failure
+if [[ $? -ne 0 ]]; then
+    mv "$record_dir/$attack_id" "$record_dir/FAIL_${attack_id}_${timestamp}"
+    echo "!!! TRAINING FAILURE !!!"
+    exit 1
+fi
 
-# echo "!!! FINISHED TRAINING !!!"
-# cd $record_dir
-    
-# tar -cf "${attack_id}_${timestamp}.tar" $attack_id && rm -rf $attack_id
+echo "!!! FINISHED TRAINING !!!"
+
+# Remove clean images from trainset to save space
+python remove_clean_imgs.py -save_dir=$record_dir
+
+cd $record_dir    
+tar -cf "${attack_id}_${timestamp}.tar" $attack_id && rm -rf $attack_id
